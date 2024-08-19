1f50e3cc68f311ebe727c1e9054b6982914b8126
wake.zheng <79523601@qq.com>
2024-08-20 00:05:17 +0800

    https代理

--- su-netty/src/main/java/com/zwk/su_netty/netty_server/http/HttpProxyServerHandler.java
+++ su-netty/src/main/java/com/zwk/su_netty/netty_server/http/HttpProxyServerHandler.java
@@ -1,12 +1,20 @@
 package com.zwk.su_netty.netty_server.http;
 
 import com.zwk.su_netty.log.LogConstant;
+import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.util.ReferenceCountUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URI;
 
 /**
  * @author zhengweikang
@@ -20,7 +28,11 @@ public class HttpProxyServerHandler extends ChannelDuplexHandler {
         LogConstant.accessLogger.info("access msg:{}\n", msg);
         if (msg instanceof FullHttpRequest) {
             FullHttpRequest request = (FullHttpRequest) msg;
-            TargetHttpRequestInitializer.connectToTargetServer(ctx, request);
+            if ("CONNECT".equalsIgnoreCase(request.method().name())) {
+                handleConnectRequest(ctx, request);
+            } else {
+                TargetHttpRequestInitializer.connectToTargetServer(ctx, request);
+            }
         } else if (ctx.channel().attr(outboundChannelKey).get() != null) {
             Channel outboundChannel = ctx.channel().attr(outboundChannelKey).get();
             outboundChannel.writeAndFlush(msg);
@@ -40,6 +52,39 @@ public class HttpProxyServerHandler extends ChannelDuplexHandler {
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         cause.printStackTrace();
         ctx.close();
+    }
+
+    private void handleConnectRequest(ChannelHandlerContext ctx, FullHttpRequest request) {
+        URI uri = URI.create(request.uri());
+        String targetHost = uri.getHost();
+        int targetPort = uri.getPort() != -1 ? uri.getPort() : 443;
+
+        // Create a new bootstrap for the TLS connection.
+        Bootstrap tlsBootstrap = new Bootstrap()
+                .group(ctx.channel().eventLoop())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        SslContext sslCtx = SslContextBuilder.forClient()
+                                .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                                .build();
+                        ch.pipeline().addLast(sslCtx.newHandler(ch.alloc()));
+                        ch.pipeline().addLast(new TargetHttpHandler(ctx, uri, request.method(), false));
+                    }
+                });
+
+        // Connect to the target host and port.
+        tlsBootstrap.connect(targetHost, targetPort).addListener((ChannelFutureListener) future -> {
+            if (future.isSuccess()) {
+                Channel outboundChannel = future.channel();
+                ctx.channel().attr(HttpProxyServerHandler.outboundChannelKey).set(outboundChannel);
+                ctx.writeAndFlush(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK));
+            } else {
+                ctx.writeAndFlush(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_GATEWAY));
+                ctx.close();
+            }
+        });
     }
 
 
